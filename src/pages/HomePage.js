3e6f979b84b9d6d46a5eebc06103c7f441f1d4d3
shecode-hue <shainkampasi@gmail.com>
2024-05-02 10:00:45 +0200

    added arff4

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div>
       <h1>Save Time, Have Accuracy, Save Money</h1>
       <p>Create Your AI Budget</p>
-      <Link to="/questions">
+      <Link to="https://colab.research.google.com/drive/1V_tf1-ab-xqkXDTuLQ8x92BOt04fdtbn?usp=sharing">
         <button>Create AI Budget</button>
       </Link>
     </div>
